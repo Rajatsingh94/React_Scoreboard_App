7f6f97514ec55f510b89ee3e4181084b7b0e4b94
Rajatsingh94 <rajat94.singh@gmail.com>
2018-01-19 02:09:15 -0500

    Counter/Stats component added!

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Stopwatch from '../components/Stopwatch';
+import Stats from '../components/Stats';
+import Counter from '../components/Counter';
+
 const INITIAL_STATE = {
   players: [
     {
@@ -75,33 +78,8 @@ Header.propTypes = {
   players: React.PropTypes.array.isRequired,
 };
 
-// Move to components/Stats.js
+//
 // -----------------------------------------------------------------------
-function Stats(props) {
-  const playerCount = props.players.length;
-  const totalPoints = props.players.reduce(function(total, player) {
-    return total + player.score;
-  }, 0);
-
-  return (
-    <table className="stats">
-      <tbody>
-        <tr>
-          <td>Players:</td>
-          <td>{playerCount}</td>
-        </tr>
-        <tr>
-          <td>Total Points:</td>
-          <td>{totalPoints}</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
-
-Stats.propTypes = {
-  players: React.PropTypes.array.isRequired,
-};
 
 // ------------------------------------------------------------------------
 
@@ -132,24 +110,7 @@ Player.propTypes = {
 
 // ----------------------------------------------------------
 
-function Counter(props) {
- return (
-   <div className="counter" >
-     <button className="counter-action decrement" onClick={() => props.onChange(-1)}>
-       -
-     </button>
-     <div className="counter-score"> {props.score} </div>
-     <button className="counter-action increment" onClick={() => props.onChange(1)}>
-       +
-     </button>
-   </div>
- );
-}
 
-Counter.propTypes = {
-  onChange: React.PropTypes.func.isRequired,
-  score: React.PropTypes.number.isRequired,
-};
 
 const AddPlayerForm = React.createClass({
   propTypes: {
