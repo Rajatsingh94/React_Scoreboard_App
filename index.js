9e98769ad9e93a10ff81a657049e85eca357ff2c
Rajatsingh94 <rajat94.singh@gmail.com>
2018-01-19 01:49:10 -0500

    Files updated/Modified!

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Scoreboard from './src/containers/Scoreboard';
+
+render(
+  <Scoreboard />,
+  document.getElementById('root')
+
+
+);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  devtool: 'inline-sourcemap',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          'react-hot',
+          'babel'
+        ]
+      }
+    ]
+  },
+
+  devServer:{
+    historyApiFallback:true,
+    contentBase:'./',
+    port:3000
+  }
+};
